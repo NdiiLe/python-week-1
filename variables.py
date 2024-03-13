7df122c13e1c8a6e80c069863b9a26ee341dc4c5
John Doe <johndoe@email.com>
2024-03-13 09:49:34 +0200

    Variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+# x is a type of an integer
+# x is defined as a type of string 
+
+x = 10
+x = "NdiiLe" 
+
+print(x)
+
+
+#specifying data type of a variable
+
+x = str (10)
+y = int (10)
+z = float (10)
+
+print (x)
+print (z)
