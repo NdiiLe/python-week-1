4d37d8b4bdbe5b07058708a95216c7ea851a4958
John Doe <johndoe@email.com>
2024-03-13 09:33:23 +0200

    Factorial

--- factorial.py
+++ factorial.py
@@ -10,3 +10,17 @@ def factorial (x):
     result = factorial(number)
     
     print("the factorial of", number, "is", result)
+    
+    
+    
+    
+    # Python code to demonstrate naive method
+# to compute factorial
+number = 23
+fact = 1
+
+for i in range(1, number+1):
+	fact = fact * i
+
+print("The factorial of 23 is : ", end="")
+print(fact)
